0944d960e4f2fb2b8a387b7efd3ee765e57b5946
choipasak <chlwjdgus822@gmail.com>
2023-11-04 13:32:41 +0900

    (화면 입력 값 -> DB 연결 + 저장) test 성공

--- TodoListProject/src/main/java/com/spring/todoproject/user/controller/UserController.java
+++ TodoListProject/src/main/java/com/spring/todoproject/user/controller/UserController.java
@@ -1,39 +1,50 @@
 package com.spring.todoproject.user.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.spring.todoproject.user.entity.User;
+import com.spring.todoproject.user.dto.UserInsertInfoRequestDTO;
 import com.spring.todoproject.user.service.IUserService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/todok")
 @RequiredArgsConstructor
 public class UserController {
 	
 	@Autowired
 	private final IUserService service;
 	
+	// @RequestMapping(value = "/test1", method = RequestMethod.GET)
 	@GetMapping("/test1")
-	public String insertUserDate() {
-		service.insertUserData(User.builder()
-				.userId("bbb")
-				.userPw("aaa")
-				.name("최정현")
-				.nickName("aaa")
-				.email1("aaa")
-				.email2("aaa")
-				.build());
+	public String insertUserDate(HttpServletRequest request, Model model, UserInsertInfoRequestDTO dto) {
+		// service.insertUserData();
+		
+		String id = request.getParameter("userId");
+		String pw = request.getParameter("userPw");
+		String name = request.getParameter("name");
+		String nickName = request.getParameter("nickName");
+		String email1= request.getParameter("email1");
+		String email2 = request.getParameter("email2");
+//		System.out.println("입력한 id값" + id);
+		
+		dto.setUserId(id);
+		dto.setUserPw(pw);
+		dto.setName(name);
+		dto.setNickName(nickName);
+		dto.setEmail1(email1);
+		dto.setEmail2(email2);
+		
+		service.insertUserData(dto);
 		
 		return "todok/test1";
 	}
-	
-	@PostMapping
 	
 	
 }
